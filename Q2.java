46ca716be5ee1b782e464a9ae586db07d89893f2
Aashish Chilwal <aashish001chilwal@gmail.com>
2021-02-12 11:30:48 -0700

    Q2

--- Q2.java
+++ Q2.java
@@ -1,7 +1,14 @@
-// Boilerplate for Lab Question 2
-
 public class Q2 {
-    public static void main(String[] args) {
-        // Your solution here
+
+	public static void main(String[] args) {
+        int w = 7;
+        int h = 9;
+       int area = w * h;
+       int perimeter = 2*(l+w);
+       System.out.println("The area is: " + area );
+       System.out.println("The perimeter is: " + perimeter);
+
+
     }
+  
 }

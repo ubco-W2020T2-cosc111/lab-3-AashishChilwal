5ebf0d79d6e456418ccf8963cb127fd726000dbc
Aashish Chilwal <aashish001chilwal@gmail.com>
2021-02-12 16:08:31 -0700

    Q4

--- Q4.java
+++ Q4.java
@@ -1,7 +1,20 @@
-// Boilerplate for Lab Question 4
+import java.util.Scanner; 
 
 public class Q4 {
-    public static void main(String[] args) {
-        // Your solution here
-    }
+  public static void main(String[] args) {
+    Scanner calc = new Scanner(System.in);
+    double v_i,v_f,t;
+    
+    // Enter username and press Enter
+    System.out.println("Enter intial velocity m/s:"); 
+    v_i = calc.nextDouble();   
+    System.out.println("Enter final velocity in m/s:"); 
+    v_f = calc.nextDouble();   
+    System.out.println("Enter time in seconds:"); 
+    t = calc.nextDouble();   
+    double a = (v_f - v_i) / (t-0);
+
+    System.out.println("The average acceleration is: " + a + " m/s^2.");        
+  }
 }
+

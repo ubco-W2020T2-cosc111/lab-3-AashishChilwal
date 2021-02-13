f25fecffa9bfc71a1e94e6d9679ba3c25b809238
Aashish Chilwal <aashish001chilwal@gmail.com>
2021-02-12 17:36:51 -0700

    Q3

--- Q3.java
+++ Q3.java
@@ -2,6 +2,11 @@
 
 public class Q3 {
     public static void main(String[] args) {
-        // Your solution here
+        double t = 12.0; 
+        double n = 2.0;
+        double v_i = 0.0;
+        double g = 9.81;
+        double dist = v_i*t + (g*t*t)/2;
+        System.out.println("Distance after an object falls 12 seconds is:" + dist+" m.");     
     }
 }
